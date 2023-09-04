881f076ac8aaea9d6bdf93dadeddc66925e36d3e
Chikondi Nkhoma <61491585+NkhomaChikondi@users.noreply.github.com>
2023-09-04 08:10:23 +0200

    created data manager and interface and then map them togehter using dependency injection

--- Services/EncodeMessageService.cs
+++ Services/EncodeMessageService.cs
@@ -11,7 +11,29 @@ namespace SpyGame.Services
     {
         public string Encoding(string message)
         {
-            throw new NotImplementedException();
+            // Define the digits to be used for "Jump the Five"
+             char[] numberMapping = { '0', '2', '3', '4', '5', '5', '7', '8', '9', '0' };
+
+            // Create a StringBuilder to build the encoded message
+            StringBuilder encodedMessage = new StringBuilder();
+
+            foreach (char c in message)
+            {
+                // check if
+                if (char.IsDigit(c))
+                {
+                    // If the character is a digit, apply the mapping
+                    int digit = c - '0';
+                    encodedMessage.Append(numberMapping[digit]);
+                }
+                else
+                {
+                    // If not a digit, append the character as is
+                    encodedMessage.Append(c);
+                }
+            }
+
+            return encodedMessage.ToString();
         }
     }
 }
